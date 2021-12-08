a8df1ddcff69b48035b993c3a9dbc286885df5e3
Sky Coder <khursidapervin@gmail.com>
2021-12-08 23:02:24 +0600

    add service provider to package

---
+++ src/LaravelFilesaverServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Skycoder\LaravelFilesaver;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFilesaverServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
