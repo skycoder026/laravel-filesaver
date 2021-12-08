66f909caa7e9335b4fc6b7924d26f4eb85836432
Sky Coder <khursidapervin@gmail.com>
2021-12-08 22:37:58 +0600

    init package

---
+++ filesaver/src/Filesaver.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Skycoder\LaravelFilesaver;
+
+class Filesaver
+{
+    public function greet(String $name)
+    {
+        return 'Hello ' . $name . '! Welcome to Mynotepaper.com';
+    }
+}
